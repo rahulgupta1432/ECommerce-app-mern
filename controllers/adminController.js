0cc58ecc188c8c5531aae3acec9b4e91ec63cd45
Rahul Gupta <rahulkumar058790@gmail.com>
2024-10-13 04:31:14 +0530

    Refactor UserController: Add getUserOrders API of user order Refactor paymentController: Handle empty cart for paymentForOrder API Refactor adminController and orderModel: Add getOrderById API and paymentMode field Added new Controller i.e. User Controller and setup new routing Feature: Added Payment Gateway API Integrated and rest of User API

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -20,6 +20,10 @@ export const getAllOrders=async(req,res,next)=>{
         getOrders = await Orders.find()
             .populate({
                 path: "product"
+            }).populate({
+                path:'buyer',
+                select:'-password -isAdmin',
+                match:userId?{_id:userId}:undefined
             })
             .limit(limit)
             .skip(skip)
@@ -86,3 +90,31 @@ export const getOrderById=async(req,res,next)=>{
         return next(new ErrorHandler(error.message,500));
     }
 }
+
+
+export const updateOrderStatus=async(req,res,next)=>{
+    try{
+        const {orderId,status}=req.body;
+        if(!orderId||!status){
+            return next(new ErrorHandler("Please Provide Order Id and Status",400));
+        }
+        const order=await Orders.findByIdAndUpdate(orderId,{
+            status
+        });
+        if(!order){
+            return next(new ErrorHandler("Order Not Found",404));
+        }
+        sendResponse({
+            res,
+            message:"Order Updated Successfully",
+            data:order
+        })
+
+    }catch(error){
+        return next(new ErrorHandler(error.message,500));
+    }
+}
+
+
+
+
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Auth, { Admin } from "../middleware/authMiddleware.js";
-import { getAllOrders, getOrderById } from "../controllers/adminController.js";
+import { getAllOrders, getOrderById, updateOrderStatus } from "../controllers/adminController.js";
 
 
 const router=express.Router();
@@ -8,6 +8,8 @@ const router=express.Router();
 router.get('/get-orders',getAllOrders);
 
 router.get('/get/product-order',getOrderById);
+
+router.put('/update-status/order',updateOrderStatus);
 
 
 
